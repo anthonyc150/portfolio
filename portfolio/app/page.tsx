ff7ecb5d10d3a70b958e0a0872e9d88566346a62
Anthony <antchinchilla34@gmail.com>
2026-06-30 18:43:22 -0400

    added name and title on page along with picture

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,13 +1,27 @@
+import Image from "next/image"
+
 export default function Home(){
   return(
-    <main className="min-h-screen" 
-    >
+    <main className="min-h-screen">
       {/*
-    <div>
-      <h1 className="font-semibold text-4xl">Anthony Chinchilla</h1>
-      <p>Software Engineer</p>
+        sizing scale for text
+        text-sm = small
+        text-base default
+        text-lg = large 
+        text-xl = extra-large
+        text-2xl through text-9xl gets progressively bigger*/}
+    <div className="relative">
+      <h1 className="absolute top-47 left-225 font-semibold text-5xl">Anthony Chinchilla</h1>
+      <p className="absolute top-59 left-225 text-xl">Software Engineering & Data Science</p>
+      <Image
+      src="/linkedin photo.jpeg"
+      alt="Anthony Chinchilla"
+      width={200}
+      height={200}
+      className="absolute top-20 left-170 rounded-full">
+      </Image>
     </div>
-    */}
+    
     </main>
   )
 }
