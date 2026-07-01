0808a5f060484f64a5090098f0a0c389bd8fd76d
Anthony <antchinchilla34@gmail.com>
2026-06-30 22:18:24 -0400

    added the about section in the intro and added the vertical appearing animation

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image"
+import AnimatedContent from '@/app/components/verticalAni'
+
 
 export default function Home(){
   return(
@@ -10,18 +12,42 @@ export default function Home(){
         text-lg = large 
         text-xl = extra-large
         text-2xl through text-9xl gets progressively bigger*/}
-    <div className="relative">
-      <h1 className="absolute top-47 left-225 font-semibold text-5xl">Anthony Chinchilla</h1>
-      <p className="absolute top-59 left-225 text-xl">Software Engineering & Data Science</p>
+
+    <AnimatedContent
+      distance={100}
+      direction="vertical"
+      reverse={false}
+      duration={0.8}
+      ease="power3.out"
+      initialOpacity={0}
+      animateOpacity
+      scale={1}
+      threshold={0.1}
+      delay={0}>
+
+    <div>
+    <div className="flex flex-col items-center justify-center gap-5 pt-15 px-20">
+      <div className="flex items-center gap-5">
       <Image
-      src="/linkedin photo.jpeg"
-      alt="Anthony Chinchilla"
-      width={200}
-      height={200}
-      className="absolute top-20 left-170 rounded-full">
-      </Image>
+        src="/linkedin photo.jpeg"
+        alt="Anthony Chinchilla"
+        width={200}
+        height={200}
+        className="rounded-full"
+      />
+      <div className="flex flex-col">
+        <h1 className="font-semibold text-5xl">Anthony Chinchilla</h1>
+        <p className="text-xl">Software Engineering & Data Science</p>
+      </div>
+      </div>
+      <div>
+        <p className="font-semibold text-lg px-83">Hi! I'm Anthony, and I am a Computer Science student at the University at Buffalo.
+          I love problem solving and have a passion for software engineering and data science. I'm actively expanding my skills through new 
+          projects and opportunities.</p>
+      </div>
     </div>
-    
+    </div>
+    </AnimatedContent>
     </main>
   )
 }
